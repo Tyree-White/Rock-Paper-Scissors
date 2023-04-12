890c9e4ffd2f5cd92aec40a5a76c858811414497
Tyree <tyreewhite01102006@gmail.com>
2023-04-12 11:45:45 -0500

    Convert number to rock paper or scissors in getComputerChoice()

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getComputerChoice () {
-    computerChoice = Math.floor((Math.random() * 3));
+    randomNumber = Math.floor((Math.random() * 3));
+    let computerChoice;
+    if (randomNumber === 0) {
+      computerChoice = "Rock";
+    } else if (randomNumber === 1) {
+      computerChoice = "Paper";
+    } else {
+      computerChoice = "Scissors";
+    }
     return computerChoice;
 }
