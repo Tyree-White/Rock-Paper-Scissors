f00ea0a6b65a12fde3661ac7ce47c9b427556537
Tyree <tyreewhite01102006@gmail.com>
2023-04-28 11:46:53 -0500

    Add comments for explanation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Used to randomly select Rock, Paper, or Scissors.
+// Used to randomly select Rock, Paper, or Scissors
 function getComputerChoice() {
     randomNumber = Math.floor((Math.random() * 3));
     let computerChoice;
-    // Converts 0, 1, or 2 into Rock, Paper, or Scissors.
+    // Converts 0, 1, or 2 into Rock, Paper, or Scissors
     if (randomNumber === 0) {
       computerChoice = "ROCK";
     } else if (randomNumber === 1) {
@@ -14,9 +14,12 @@ function getComputerChoice() {
 }
 
 function playGame() {
+    // Stores user and computer choices
     let computerChoice = getComputerChoice();
     let userChoice = this.value;
+    // Reference to result division
     const result = document.querySelector('.result')
+    // Changes text content of result division based on conditions
     if (userChoice === computerChoice) {
         result.textContent = `The battle has resulted in a draw! Your enemy chose ${computerChoice}! You chose ${userChoice}!`;
     } else if (userChoice === "ROCK" && computerChoice === "SCISSORS") {
@@ -34,10 +37,12 @@ function playGame() {
     }
 }
 
+// Stores user and computer score
 let userScore = 0;
 let compScore = 0;
 
 function war() {
+  // References to division
   const result = document.querySelector('.result').textContent;
   const userPoints = document.querySelector('.userPoints');
   const computerPoints = document.querySelector('.compPoints');
